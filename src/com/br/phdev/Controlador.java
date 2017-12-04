46689317706d04bb0ba9aca4342dc4f42369545c
elfa1 <henrique.phgb@gmail.com>
2017-12-04 12:50:28 -0300

    Aprimorando movimentos

--- src/com/br/phdev/Controlador.java
+++ src/com/br/phdev/Controlador.java
@@ -116,11 +116,11 @@ public class Controlador {
 
             switch (comandos[index++]) {
                 case -2:
+                    sleep(150);
                     pernas[PERNA_1].sleep(150);
                     pernas[PERNA_2].sleep(150);
                     pernas[PERNA_3].sleep(150);
-                    pernas[PERNA_4].sleep(150);
-                    sleep(150);
+                    pernas[PERNA_4].sleep(150);                    
                     break;
                 case 0:
                     System.out.println("LEVANTANDO PERNA 1");
