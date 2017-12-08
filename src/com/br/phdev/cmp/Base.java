78d83d8dba1e6843998ac2d96ffa9ce43b56fb0e
elfa1 <henrique.phgb@gmail.com>
2017-12-08 20:44:37 -0300

    Aprimorando movimentos

--- src/com/br/phdev/cmp/Base.java
+++ src/com/br/phdev/cmp/Base.java
@@ -30,7 +30,7 @@ public class Base extends Componente {
         int valor2 = super.getServo().getMovMax();
         int valor3 = (valor1 > valor2 ? valor1 - valor2 : valor2 - valor1) / 2;
         invr = super.getServo().getMovMax() > super.getServo().getMovMin();
-        if (!invr)
+        if (invr)
             super.getServo().setPosicao(super.posInicial + valor3);
         else
             super.getServo().setPosicao(super.posInicial - valor3);
