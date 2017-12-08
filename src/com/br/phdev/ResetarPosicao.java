9c2fa61293f494332a9258eb005af04309b19080
elfa1 <henrique.phgb@gmail.com>
2017-12-08 14:10:16 -0300

    Aprimorando movimentos

--- src/com/br/phdev/ResetarPosicao.java
+++ src/com/br/phdev/ResetarPosicao.java
@@ -17,21 +17,26 @@ import com.pi4j.io.i2c.I2CFactory;
  * @author PH
  */
 public class ResetarPosicao {
-    
+
     private final int PERNA_1 = 0; // PERNA TRASEIRA DIREITA
     private final int PERNA_2 = 1; // PERNA TRASEIRA ESQUERDA
     private final int PERNA_3 = 2; // PERNA DIANTEIRA DIREITA
     private final int PERNA_4 = 3; // PERNA DIANTEIRA ESQUERDA
-    
+
     private Perna[] pernas;
-    
+
     private PCA9685 modulo;
-    
-    public ResetarPosicao() throws I2CFactory.UnsupportedBusNumberException{
-        
+
+    public ResetarPosicao() throws I2CFactory.UnsupportedBusNumberException {
+
         modulo = new PCA9685();
         modulo.setPWMFreq(60);
-        
+
+        for (int i = 0; i < 16; i++) {
+            modulo.setPWM(i, 0, 0);
+            delay(200);
+        }
+
         pernas = new Perna[4];
         pernas[PERNA_1] = new Perna(new Base(modulo, 5, 385, new Femur(modulo, 4, 398, new Tarso(modulo, 0, 310))));
         pernas[PERNA_1].getBase().getFemur().getTarso().setLimites(200, 420);
@@ -53,9 +58,9 @@ public class ResetarPosicao {
         pernas[PERNA_4].getBase().getFemur().setLimites(540, 295);
         pernas[PERNA_4].getBase().setLimites(290, 490);
     }
-    
-    public void resetar(){
-        for (Perna perna : pernas){
+
+    public void resetar() {
+        for (Perna perna : pernas) {
             perna.getBase().resetarPosicao();
             perna.getBase().mover();
             delay(300);
@@ -67,7 +72,7 @@ public class ResetarPosicao {
             delay(300);
         }
     }
-    
+
     private void delay(int tempo) {
         try {
             Thread.sleep(tempo);
@@ -80,7 +85,6 @@ public class ResetarPosicao {
 
         ResetarPosicao res = new ResetarPosicao();
         res.resetar();
-        
 
     }
 
