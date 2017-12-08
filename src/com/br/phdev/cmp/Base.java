6b7628ed74c21a7d0d7f481e4f5c1fb0de9bf9d3
elfa1 <henrique.phgb@gmail.com>
2017-12-08 20:32:38 -0300

    Aprimorando movimentos

--- src/com/br/phdev/cmp/Base.java
+++ src/com/br/phdev/cmp/Base.java
@@ -13,15 +13,29 @@ public class Base extends Componente {
     }
 
     public void levantarMetade() {
+        boolean invr;
         int valor1 = super.getServo().getPosicao();
         int valor2 = super.getServo().getMovMax();
+        int valor3 = (valor1 > valor2 ? valor1 - valor2 : valor2 - valor1) / 2;
+        invr = super.getServo().getMovMax() > super.getServo().getMovMin();
+        if (!invr)
+            super.getServo().setPosicao(super.posInicial - valor3);
+        else
+            super.getServo().setPosicao(super.posInicial + valor3);
+        
         super.levantar((valor1 > valor2 ? valor1 - valor2 : valor2 - valor1) / 2);
     }
 
     public void abaixarMetade() {
+        boolean invr;
         int valor1 = super.getServo().getPosicao();
         int valor2 = super.getServo().getMovMax();
-        super.abaixar((valor1 > valor2 ? valor1 - valor2 : valor2 - valor1) / 2);
+        int valor3 = (valor1 > valor2 ? valor1 - valor2 : valor2 - valor1) / 2;
+        invr = super.getServo().getMovMax() > super.getServo().getMovMin();
+        if (!invr)
+            super.getServo().setPosicao(super.posInicial + valor3);
+        else
+            super.getServo().setPosicao(super.posInicial - valor3);
     }
 
     public Femur getFemur() {
