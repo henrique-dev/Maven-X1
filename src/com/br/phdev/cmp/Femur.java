6dece23eb74a66d1cefd76bbf537a1c0cdb6a106
elfa1 <henrique.phgb@gmail.com>
2017-12-08 18:39:03 -0300

    Aprimorando movimentos

--- src/com/br/phdev/cmp/Femur.java
+++ src/com/br/phdev/cmp/Femur.java
@@ -30,17 +30,25 @@ public class Femur extends Componente {
         tarso.levantar(mov);
     }
 
-    public void esticar() {
+    public void esticar() {        
+        boolean invr;
         int posi = super.getServo().getPosicao();
         int posf = super.getServo().getMovMin();
         int mov = posi > posf ? posi - posf : posf - posi;
-        //super.abaixar(mov / 3);
-        super.getServo().setPosicao(super.posInicial - (mov)/3);
+        invr = (super.getServo().getMovMax() > super.getServo().getMovMin());
+        if (invr)
+            super.getServo().setPosicao(super.posInicial - (mov)/3);
+        else
+            super.getServo().setPosicao(super.posInicial - (mov)/3);
 
+        invr = (tarso.getServo().getMovMax() > tarso.getServo().getMovMin());
         int valor1 = tarso.getServo().getPosicao();
         int valor2 = tarso.getServo().getMovMax();
         int valor3 = (valor1 > valor2 ? valor1 - valor2 : valor2 - valor1) / 2;
-        tarso.getServo().setPosicao(tarso.posInicial - valor3);        
+        if (invr)
+            tarso.getServo().setPosicao(tarso.posInicial - valor3);        
+        else
+            tarso.getServo().setPosicao(tarso.posInicial + valor3);        
     }
 
     @Override
