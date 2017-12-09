8e57322b40af4baa4a6a085399642b58e1e22681
elfa1 <henrique.phgb@gmail.com>
2017-12-09 10:54:01 -0300

    Aprimorando movimentos

--- src/com/br/phdev/Controlador.java
+++ src/com/br/phdev/Controlador.java
@@ -362,6 +362,7 @@ public class Controlador {
                     pernas[PERNA_2].setDescendobase(false);
                     pernas[PERNA_3].setDescendobase(false);
                     pernas[PERNA_4].setDescendobase(false);
+                    break;
                 case 150:
                     System.out.println("ALGORITMO DE PASSO V1");
                     preMsg = "3 53-50-54-52-5 29 55 78-25-28-27-75-79-77-53 80 4 30-0-3-2";
