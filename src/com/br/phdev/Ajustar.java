15fdd9c17c723b556fd41cf4d97abc26239cbb76
elfa1 <henrique.phgb@gmail.com>
2017-12-09 21:23:13 -0300

    Aprimorando movimentos

--- src/com/br/phdev/Ajustar.java
+++ src/com/br/phdev/Ajustar.java
@@ -9,20 +9,13 @@ import com.br.phdev.driver.PCA9685;
 import com.pi4j.io.i2c.I2CFactory;
 import com.pi4j.io.i2c.I2CBus;
 import com.pi4j.io.i2c.I2CFactory;
+import com.sun.javafx.fxml.expression.Expression;
 
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Ajustar {
-
-    private final int PERNA_1 = 0; // PERNA TRASEIRA DIREITA
-    private final int PERNA_2 = 1; // PERNA TRASEIRA ESQUERDA
-    private final int PERNA_3 = 2; // PERNA DIANTEIRA DIREITA
-    private final int PERNA_4 = 3; // PERNA DIANTEIRA ESQUERDA
-
-    private Perna[] pernas;
-
-    private MainThread thread;
+public class Ajustar {    
+    
     private PCA9685 modulo;
 
     public Ajustar() throws I2CFactory.UnsupportedBusNumberException {
@@ -34,68 +27,12 @@ public class Ajustar {
             modulo.setPWM(i, 0, 0);
             delay(200);
         }
-
-        pernas = new Perna[4];
-        pernas[PERNA_1] = new Perna(new Base(modulo, 5, 385, new Femur(modulo, 4, 428, new Tarso(modulo, 0, 340))));
-        pernas[PERNA_1].getBase().getFemur().getTarso().setLimites(200, 420);
-        pernas[PERNA_1].getBase().getFemur().setLimites(520, 275);
-        pernas[PERNA_1].getBase().setLimites(280, 490);
-
-        pernas[PERNA_2] = new Perna(new Base(modulo, 3, 415, new Femur(modulo, 2, 300, new Tarso(modulo, 1, 195))));
-        pernas[PERNA_2].getBase().getFemur().getTarso().setLimites(340, 130);
-        pernas[PERNA_2].getBase().getFemur().setLimites(185, 475);
-        pernas[PERNA_2].getBase().setLimites(310, 520);
-
-        pernas[PERNA_3] = new Perna(new Base(modulo, 7, 395, new Femur(modulo, 6, 313, new Tarso(modulo, 14, 195))));
-        pernas[PERNA_3].getBase().getFemur().getTarso().setLimites(360, 150);
-        pernas[PERNA_3].getBase().getFemur().setLimites(220, 465);
-        pernas[PERNA_3].getBase().setLimites(300, 490);
-
-        pernas[PERNA_4] = new Perna(new Base(modulo, 8, 390, new Femur(modulo, 9, 448, new Tarso(modulo, 15, 333))));
-        pernas[PERNA_4].getBase().getFemur().getTarso().setLimites(185, 420);
-        pernas[PERNA_4].getBase().getFemur().setLimites(540, 295);
-        pernas[PERNA_4].getBase().setLimites(290, 490);
-    }
-
-    public void resetar() {
-        for (Perna perna : pernas) {
-            perna.getBase().resetarPosicao();
-            perna.getBase().mover();
-            delay(300);
-            perna.getBase().getFemur().resetarPosicao();
-            perna.getBase().getFemur().mover();
-            delay(300);
-            perna.getBase().getFemur().getTarso().resetarPosicao();
-            perna.getBase().getFemur().getTarso().mover();
-            delay(300);
-        }
-    }
-
-    public void movimentar() {
-        for (Perna perna : pernas) {
-            perna.getBase().mover();
-            perna.getBase().getFemur().mover();
-            perna.getBase().getFemur().getTarso().mover();
-        }
-    }
-
-    public void setPos(int ch, int pos) {
-        for (Perna perna : pernas) {
-            if (perna.getBase().getServo().getCanal() == ch) {
-                perna.getBase().getServo().setPosicao(pos);
-            } else if (perna.getBase().getFemur().getServo().getCanal() == ch) {
-                perna.getBase().getFemur().getServo().setPosicao(pos);
-            } else if (perna.getBase().getFemur().getTarso().getServo().getCanal() == ch) {
-                perna.getBase().getFemur().getTarso().getServo().setPosicao(pos);
-            }
-        }
+        
     }
     
-    public void iniciar(){
-        if (thread == null)
-            thread = new MainThread();
-        thread.start();
-    }
+    public void setServoPos(int ch, int pos){
+        
+    }        
 
     private void delay(int tempo) {
         try {
@@ -103,22 +40,7 @@ public class Ajustar {
         } catch (InterruptedException ex) {
             ex.printStackTrace();
         }
-    }    
-
-    public class MainThread extends Thread {
-
-        @Override
-        public void run() {
-            while (true) {
-                for (Perna perna : pernas) {
-                    perna.getBase().mover();
-                    perna.getBase().getFemur().mover();
-                    perna.getBase().getFemur().getTarso().mover();
-                }
-            }
-        }
-
-    }
+    }        
 
     public static void main(String[] args) throws I2CFactory.UnsupportedBusNumberException {
 
@@ -129,8 +51,7 @@ public class Ajustar {
         int servoChannel = 0;
         int servoPos = 0;
 
-        aj.resetar();
-        aj.iniciar();
+        
 
         while (true) {
             System.out.println("AJUSTANDO SERVO\n\n");
@@ -139,8 +60,8 @@ public class Ajustar {
             System.out.println("Informe a posicao para o servo: ");
             servoPos = entrada.nextInt();
             System.out.println("Movendo para " + servoPos);
-            aj.setPos(servoChannel, servoPos);            
-
+            if (servoPos >= 150 && servoPos <= 600)
+                aj.setServoPos(servoChannel, servoPos);                               
         }
 
         // 375 servo middle
