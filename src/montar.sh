ea502c2dc8bab2afa8540080eb7d4311491349e3
elfa1 <henrique.phgb@gmail.com>
2017-12-10 11:19:10 -0300

    Aprimorando movimentos

--- src/montar.sh
+++
@@ -1,3 +0,0 @@
-echo "EXECUTANDO TESTE DE SERVO MAVEN"
-
-sudo sudo jar cvmf manifest.mf maven.jar com/br/phdev/*.class com/br/phdev/driver/*.class com/br/phdev/cmp/*.class com/br/phdev/conexao/*.class
