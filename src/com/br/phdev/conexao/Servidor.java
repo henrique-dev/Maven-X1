7092018e95a4fb5f86a54f0c97a405e84efea5c3
elfa1 <henrique.phgb@gmail.com>
2017-12-10 01:27:17 -0300

    Aprimorando movimentos

--- src/com/br/phdev/conexao/Servidor.java
+++ src/com/br/phdev/conexao/Servidor.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -35,7 +36,7 @@ public class Servidor extends Thread{
     
     private Socket iniciar(){
         
-        try{            
+        try{                        
             server = new ServerSocket(PORTA);
             while(true){
                 Socket con = server.accept();
@@ -45,7 +46,15 @@ public class Servidor extends Thread{
         }
         catch(IOException e){
             e.printStackTrace();
+            try{
+                sleep(2000);
+                iniciar();
+            }
+            catch(InterruptedException ex){
+                ex.printStackTrace();
+            }            
         }
+        
         return null;
     }
     
@@ -65,10 +74,14 @@ public class Servidor extends Thread{
             try{
                 System.out.println("Fechando servidor");
                 controlador.parar();                
-                server.close();
+                server.close();                
                 server = null;
+                sleep(2000);
             }
             catch(IOException e){
+                e.printStackTrace();
+            }
+            catch(InterruptedException e){
                 e.printStackTrace();
             }
         }
