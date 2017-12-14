3fb570e89ac5ea997df5fd98ccfb7c584b2b859e
elfa1 <henrique.phgb@gmail.com>
2017-12-14 14:33:51 -0300

    Reajustando diretorios

---
+++ Raspberry/src/com/br/phdev/cmp/Componente.java
@@ -0,0 +1,91 @@
+package com.br.phdev.cmp;
+
+import com.br.phdev.driver.PCA9685;
+
+public class Componente implements Membro{
+    
+    //private ThreadServo thread;
+        
+    protected Servo servo;  
+    protected int posInicial;
+    
+    public Componente(PCA9685 modulo, int servoCanal, int posInicial){
+        this.servo = new Servo(modulo, servoCanal, posInicial);       
+    }
+    
+    public void resetarPosicao(){        
+        this.servo.setPosicao(posInicial);
+        //this.servo.mover();
+    }
+    
+    public Componente getInstance(){
+        return this;
+    }
+    
+    public void levantar(){        
+        this.servo.setPosicao(this.servo.getMovMax());        
+        //this.servo.mover();
+    }
+    
+    public void levantar(int pos){
+        if (servo.getMovMax() > servo.getMovMin())
+            this.servo.setPosicao(servo.getPosicao() + pos);
+        else
+            this.servo.setPosicao(servo.getPosicao() - pos);
+    }
+    
+    public void abaixar(){
+        this.servo.setPosicao(this.servo.getMovMin());
+        //this.servo.mover();
+    }
+    
+    public void abaixar(int pos){
+        if (servo.getMovMax() > servo.getMovMin())
+            this.servo.setPosicao(servo.getPosicao() - pos);
+        else
+            this.servo.setPosicao(servo.getPosicao() + pos);
+    }
+    
+    public void pararMovimento(){
+        //this.servo.mover(0);
+        this.servo.setPosicao(0);
+    }        
+    
+    protected void setServo(Servo servo){
+        this.servo = servo;
+    }
+    
+    protected Servo getServo(){
+        return this.servo;
+    }
+    
+    public void mover(){
+        this.servo.mover();
+    }
+    
+    public void mover(int posicao){
+        this.servo.mover(posicao);
+    }
+
+    
+    public void setLimites(int movMax, int movMin){
+        this.servo.setMovMin(movMin);
+        this.servo.setMovMax(movMax);
+    }        
+    
+    public int getPos(){
+        return servo.getPosicao();
+    }
+
+    public int getPosInicial() {
+        return posInicial;
+    }
+
+    public void setPosInicial(int posInicial) {
+        this.posInicial = posInicial;
+    }
+    
+    
+        
+    
+}
